91a1e72e618e3ec357bce6f30708384fb3d8b898
Libor Váchal <libor.vachal@heureka.cz>
2024-04-01 14:55:00 +0200

    feat: use read instead of list in KV to not hit free KV pricing limits

--- clouflare-worker/beacon-alerts-opsgenie-consumer.js
+++ clouflare-worker/beacon-alerts-opsgenie-consumer.js
@@ -4,27 +4,57 @@ export default {
         return new Response("Method not allowed", { status: 405 });
       }
   
-      const json = await request.json();
+      if (!request.headers.get("opsgenie-one-account-hash") || request.headers.get("opsgenie-one-account-hash") !== env.OPSGENIE_ONE_ACCOUNT_HASH) {
+        return new Response(`'opsgenie-one-account-hash' is invalid or missing`, {status: 403});
+      }
+  
+      let json;
+      try {
+        json = await request.json();
+      } catch (error) {
+        return new Response(`Malformated or missing event json in request body.`, {status: 400})
+      }
   
       const { alert, action } = json;
   
       const kvNamespace = "beacon-alerts-kv";
-      const kvKey = alert.alertId;
-      const kvValue = action;
+      const kvKey = "activeAlerts";
   
       const kvTtl = 2 * 60 * 60; // 2 hours in seconds
   
       try {
+        const activeAlertsValue = await env.BEACON_ALERTS_KV.get(kvKey)
   
         if (action === "Create" || action === "UnAcknowledge") {
-          await env.BEACON_ALERTS_KV.put(kvKey, kvValue, {
-            expirationTtl: kvTtl,
-            namespace: kvNamespace,
-          });
+  
+          // Create KV activeAlerts if it doesnt exist (only on initilazation)
+          if (!activeAlertsValue) {
+            await env.BEACON_ALERTS_KV.put(kvKey, JSON.stringify([alert.alertId]), {
+              expirationTtl: kvTtl,
+              namespace: kvNamespace,
+            });
+          }
+  
+          // Update KV activeAlerts if it already exists
+          if (activeAlertsValue) {
+            const activeAlertsWithNewAlert = new Set(JSON.parse(activeAlertsValue))
+            activeAlertsWithNewAlert.add(alert.alertId)
+  
+            await env.BEACON_ALERTS_KV.put(kvKey, JSON.stringify(Array.from(activeAlertsWithNewAlert.values())), {
+              expirationTtl: kvTtl,
+              namespace: kvNamespace,
+            });
+          }
         }
   
         if (action === "Close" || action === "Acknowledge") {
-          await env.BEACON_ALERTS_KV.delete(kvKey);
+          const activeAlertsWithoutAlert = new Set(JSON.parse(activeAlertsValue))
+          activeAlertsWithoutAlert.delete(alert.alertId)
+  
+          await env.BEACON_ALERTS_KV.put(kvKey, JSON.stringify(Array.from(activeAlertsWithoutAlert)), {
+            expirationTtl: kvTtl,
+            namespace: kvNamespace,
+          });
         }
   
         return new Response("Data saved to KV", { status: 200 });
--- clouflare-worker/beacon-alerts.js
+++ clouflare-worker/beacon-alerts.js
@@ -1,29 +1,28 @@
 export default {
-    async fetch(request) {
-      const upgradeHeader = request.headers.get('Upgrade');
-  
-      console.log("delej", request.method);
-      console.log("upgradeHeader", upgradeHeader);
-  
-      if (!upgradeHeader || upgradeHeader !== 'websocket') {
-        return new Response('Expected Upgrade: websocket', { status: 426 });
-      }
-    
-      const webSocketPair = new WebSocketPair();
-      const [client, server] = Object.values(webSocketPair);
-    
-      server.accept();
-      server.addEventListener('message', event => {
-        console.log(event.data);
-      });
-  
-      const actions = ["Acknowledge", "Close", "Create", "UnAcknowledge"]
-  
-      setInterval(() => server.send(actions[actions.length * Math.random() | 0]), 5000)
-  
-      return new Response(null, {
-        status: 101,
-        webSocket: client,
-      });
-    },
-  };
+  async fetch(request, env) {
+    const upgradeHeader = request.headers.get("Upgrade");
+
+    if (!upgradeHeader || upgradeHeader !== "websocket") {
+      return new Response("Expected Upgrade: websocket", { status: 426 });
+    }
+
+    const webSocketPair = new WebSocketPair();
+    const [client, server] = Object.values(webSocketPair);
+
+    server.accept();
+    server.addEventListener("message", (event) => {
+      console.log(event.data);
+    });
+
+    setInterval(async () => {
+      const activeAlerts = await env.BEACON_ALERTS_KV.get("activeAlerts");
+
+      server.send((new Set(JSON.parse(activeAlerts))).size > 0 ? "ON" : "OFF");
+    }, 5000);
+
+    return new Response(null, {
+      status: 101,
+      webSocket: client,
+    });
+  },
+};
